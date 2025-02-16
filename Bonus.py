3b05cc96c6691e44facecb92085cd96ea9b79a4f
manaralamri <manaralamre33@gmail.com>
2025-02-16 11:41:10 +0300

    lab

---
+++ Bonus.py
@@ -0,0 +1,10 @@
+from art import text2art
+from colorama import Fore, Back, Style
+art_3 = text2art("success", font="starwars")
+print(Fore.BLUE + Style.BRIGHT,art_3)
+
+art_4 = text2art("dangerous", font="starwars")
+print(Fore.RED + Style.BRIGHT,art_4)
+
+art_5 = text2art("CODE & PYthon", font="epic")
+print(Fore.LIGHTBLACK_EX + Style.BRIGHT,art_5)
---
+++ main.py
@@ -0,0 +1,8 @@
+from art import text2art
+from colorama import Fore, Back, Style
+art_1 = text2art("BELIEVE AND ACHEIVE", font="block")
+print(Fore.CYAN,art_1)
+
+art_2 = text2art("Hello", font="sub-zero")
+print(Fore.YELLOW,art_2)
+
