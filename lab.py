4f22d69f462e6fb9cfed3b71f9e470986aa83601
elaf <elafabdualrahman00@gmail.com>
2025-02-16 13:17:38 +0300

    solved lab and bonus

--- lab.py
+++ lab.py
@@ -1,4 +1,7 @@
 from art import * 
+from colorama import Fore, Back, Style
+
+#lab
 first_phrase=text2art("BELIEVE AND ACHEIVE",font='block',chr_ignore=True) # this will print the text in block font as required 
 print(first_phrase)
 
@@ -7,3 +10,9 @@ print(test1)
 
 second_phrase=text2art("HELLO",font='sub-zero',chr_ignore=True) # Return ASCII text with block font
 print(second_phrase)
+
+#bonus
+print(Fore.MAGENTA+first_phrase)
+print(Fore.RESET)
+print(Back.LIGHTBLUE_EX+test1)
+print(Fore.RED+ Back.WHITE +second_phrase)
