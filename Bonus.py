dea65775a21db03dbb90b980dd74ee9625def158
manaralamri <manaralamre33@gmail.com>
2025-02-16 12:07:40 +0300

    lab

--- Bonus.py
+++ Bonus.py
@@ -1,13 +1,13 @@
 from art import text2art
 from colorama import Fore, Back, Style
 art_3 = text2art("success", font="starwars")
-print(Fore.BLUE + Style.BRIGHT,art_3)
+print(Fore.BLUE + Style.BRIGHT + art_3)
 
 art_4 = text2art("dangerous", font="starwars")
-print(Fore.RED + Style.BRIGHT,art_4)
+print(Fore.RED + Style.BRIGHT + art_4)
 
 art_5 = text2art("CODE & PYthon", font="epic")
 print(Fore.LIGHTBLACK_EX + Style.BRIGHT,art_5)
 
 art_6 = text2art("Keep going", font="big")
-print(Fore.LIGHTBLACK_EX ,art_6)
+print(Fore.LIGHTBLACK_EX + art_6)
--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from colorama import Fore, Back, Style
 art_1=text2art("""BELIEVE
 AND 
 ACHEIVE
-""",font='block',chr_ignore=True) # Return ASCII text with block font
+""",font='block' ,chr_ignore=True) # Return ASCII text with block font
 print(art_1)
 
-art_2 = text2art("Hello", font="sub-zero")
-print(Fore.YELLOW,art_2)
+art_2 = text2art("Hello" , font="sub-zero")
+print(Fore.YELLOW + art_2)
 
