3f4e3af1695833d0bde5746a028574364e65503d
abdulrahman aidarous <abdulrahmanaid.21@gmail.com>
2025-02-16 12:09:16 +0300

    Solution for LAB_PIP_VENV and Bonus

--- LAB_PIP_VENV.py
+++ LAB_PIP_VENV.py
@@ -1,4 +1,7 @@
 from art import *
 print(text2art("Text Art"))
-tprint("BELIEVE AND ACHEIVE",font="block")
+tprint("BELIEVE",font="block")
+tprint("AND",font="block")
+tprint("ACHEIVE",font="block")
+
 tprint("HELLO",font="sub-zero")
